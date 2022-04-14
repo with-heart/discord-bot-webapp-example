cdffc8e824523d852bed71a3177340232b194e13
with-heart <with.heart+git@pm.me>
2022-04-14 06:46:56 -0400

    create deploy-commands script

---
+++ bot/scripts/deploy-commands.ts
@@ -0,0 +1,37 @@
+import {REST} from '@discordjs/rest'
+import {Routes} from 'discord-api-types/v9'
+import {clientId, guildId, token} from '../config.json'
+
+const commands: any[] = []
+
+/*
+
+This deploy script pushes our commands to the server defined by `guildId` in
+`config.json`. We only need to run this once initially, and then any time we add
+a new command.
+
+We'll eventually need to conditionally select which `Route` we're using based on
+whether we're in a production environment (`applicationCommands`) or a
+development environment (`applicationGuildCommands`, default).
+
+Since we're only worrying about development for awhile, we're defaulting to
+`applicationGuildCommands` for use with a test server. This is because
+`applicationCommands` can take up to an hour to sync commands across all servers
+using the bot, while `applicationGuildCommands` syncs the commands to a single
+server immediately.
+
+*/
+
+;(async () => {
+  // initialize discord rest client using our token
+  const rest = new REST({version: '9'}).setToken(token)
+
+  try {
+    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+      body: commands,
+    })
+    console.log('Successfully registered application commands.')
+  } catch (error) {
+    console.error(error)
+  }
+})()
